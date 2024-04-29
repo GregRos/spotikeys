6721ef2fb3306616de0e09f6a7f0be28cc810c56
GregRos <greg.r@reflectiz.com>
2024-04-29 15:27:15 +0300

    d

--- src/client/ui/framework/bindable_property.py
+++ src/client/ui/framework/bindable_property.py
@@ -3,19 +3,6 @@ from typing import Any, Callable
 from src.client.ui.framework.active_value import Disposable, Subscribable
 
 
-class BindableProperty[Value]:
-    _binding: Disposable
-
-    def __init__(self, name: str, on_change: Callable[[Value], None]):
-        self._name = name
-        self._on_change = on_change
-
-    def set(self, binding: Subscribable[Value]):
-        if self._binding:
-            self._binding.close()
-        self._binding = binding.subscribe(self._on_change)
-
-
 def bindable[Value](only_changed=False):
     def decorator[
         Self
--- src/client/ui/framework/font.py
+++ src/client/ui/framework/font.py
@@ -1,9 +1,20 @@
 from dataclasses import dataclass, field
 
+from src.client.ui.framework.active_value import ActiveValue
 from src.client.ui.framework.bindable_property import bindable
 
 
-class Font:
+@dataclass
+class FontInfo:
+    family: str = "Courier New"
+    size: int = 18
+    style: str = "normal"
+
+    def to_tk(self):
+        return (self.family, self.size, self.style)
+
+
+class Font(ActiveValue):
     _family: str = "Courier New"
     _size: int = 18
     _style: str = "normal"
